0285da1f7e9385426d208973a198399a8004f3eb
Loc Nguyen <kten1112@gmail.com>
2020-09-26 20:03:17 +0700

    (Finished) Add image to map

--- app/Http/Controllers/LandslideLocationsController.php
+++ app/Http/Controllers/LandslideLocationsController.php
@@ -15,9 +15,6 @@ class LandslideLocationsController extends Controller
         
         // Markers
         $normalLevel = LandslideLocations::where('level', 1)->where('year', 2020)->get();
-        // for($i = 0; $i < count($normalLevel); $i++ ){
-        //     print_r($i);
-        // }
         foreach($normalLevel as $n)
         {   
             foreach($images as $im)
@@ -30,7 +27,28 @@ class LandslideLocationsController extends Controller
         }
         
         $dangerLevel = LandslideLocations::where('level', 2)->where('year', 2020)->get();
+        foreach($dangerLevel as $n)
+        {   
+            foreach($images as $im)
+            {
+                if($n->location_id == $im->location_id)
+                {
+                    $n['image'] = $im->link;
+                }
+            }
+        }
+
         $veryDangerLevel = LandslideLocations::where('level', 3)->where('year', 2020)->get();
+        foreach($veryDangerLevel as $n)
+        {   
+            foreach($images as $im)
+            {
+                if($n->location_id == $im->location_id)
+                {
+                    $n['image'] = $im->link;
+                }
+            }
+        }
 
         // Normal level
         $normalArray = ['type' => 'FeatureCollection',
@@ -47,7 +65,7 @@ class LandslideLocationsController extends Controller
                     'type' => 'Feature',
                     'properties' => [
                         'hoverContent' => "<b>$n->title</b>",
-                        'detailContent' => "<div class='landslide-popup'><ul class='title'><li class='font-weight-bold' style='color: #0D47A1;'>Thông tin sạt lở</li></ul><div class='popup-content'><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Mức độ:</p><p class='p-0 my-2'>$n->level_name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Tên:</p><p class='p-0 my-2'>$n->title</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Vị trí:</p><p class='p-0 my-2'>$n->commune, $n->district, $n->province</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Ghi chú:</p><p class='p-0 my-2'>$n->name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Chiều dài (m):</p><p class='p-0 my-2'>$n->length</p></div></div></div>"
+                        'detailContent' => "<div class='landslide-popup container'><ul class='nav nav-tabs'><li class='active'><a data-toggle='tab' href='#thong-tin-sat-lo'>Thông tin sạt lở</a></li><li class='".$this->checkImageExist($n->image)."'><a data-toggle='tab' href='#hinh-anh'>Hình ảnh</a></li> </ul><div class='popup-content tab-content'><div id='thong-tin-sat-lo' class='tab-pane fade in active show'><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Mức độ:</p><p class='p-0 my-2'>$n->level_name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Tên:</p><p class='p-0 my-2'>$n->title</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Vị trí:</p><p class='p-0 my-2'>$n->commune, $n->district, $n->province</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Ghi chú:</p><p class='p-0 my-2'>$n->name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Chiều dài (m):</p><p class='p-0 my-2'>$n->length</p></div></div><div id='hinh-anh' class='tab-pane fade'><img class='w-100 d-block' style='height:200px;' alt='hinh-anh-sat-lo' src='$n->image' /></div></div>"
                     ],
                     'id' => $n->gid
                 ]);
@@ -69,7 +87,7 @@ class LandslideLocationsController extends Controller
                     'type' => 'Feature',
                     'properties' => [
                         'hoverContent' => "<b>$n->title</b>",
-                        'detailContent' => "<div class='landslide-popup'><ul class='title'><li class='font-weight-bold' style='color: #0D47A1;'>Thông tin sạt lở</li></ul><div class='popup-content'><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Mức độ:</p><p class='p-0 my-2'>$n->level_name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Tên:</p><p class='p-0 my-2'>$n->title</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Vị trí:</p><p class='p-0 my-2'>$n->commune, $n->district, $n->province</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Ghi chú:</p><p class='p-0 my-2'>$n->name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Chiều dài (m):</p><p class='p-0 my-2'>$n->length</p></div></div></div>"
+                        'detailContent' => "<div class='landslide-popup container'><ul class='nav nav-tabs'><li class='active'><a data-toggle='tab' href='#thong-tin-sat-lo'>Thông tin sạt lở</a></li><li class='".$this->checkImageExist($n->image)."'><a data-toggle='tab' href='#hinh-anh'>Hình ảnh</a></li> </ul><div class='popup-content tab-content'><div id='thong-tin-sat-lo' class='tab-pane fade in active show'><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Mức độ:</p><p class='p-0 my-2'>$n->level_name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Tên:</p><p class='p-0 my-2'>$n->title</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Vị trí:</p><p class='p-0 my-2'>$n->commune, $n->district, $n->province</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Ghi chú:</p><p class='p-0 my-2'>$n->name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Chiều dài (m):</p><p class='p-0 my-2'>$n->length</p></div></div><div id='hinh-anh' class='tab-pane fade'><img class='w-100 d-block' style='height:200px;' alt='hinh-anh-sat-lo' src='$n->image' /></div></div>"
                     ],
                     'id' => $n->gid
                 ]);
@@ -91,7 +109,7 @@ class LandslideLocationsController extends Controller
                     'type' => 'Feature',
                     'properties' => [
                         'hoverContent' => "<b>$n->title</b>",
-                        'detailContent' => "<div class='landslide-popup'><ul class='title'><li class='font-weight-bold' style='color: #0D47A1;'>Thông tin sạt lở</li></ul><div class='popup-content'><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Mức độ:</p><p class='p-0 my-2'>$n->level_name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Tên:</p><p class='p-0 my-2'>$n->title</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Vị trí:</p><p class='p-0 my-2'>$n->commune, $n->district, $n->province</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Ghi chú:</p><p class='p-0 my-2'>$n->name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Chiều dài (m):</p><p class='p-0 my-2'>$n->length</p></div></div></div>"
+                        'detailContent' => "<div class='landslide-popup container'><ul class='nav nav-tabs'><li class='active'><a data-toggle='tab' href='#thong-tin-sat-lo'>Thông tin sạt lở</a></li><li class='".$this->checkImageExist($n->image)."'><a data-toggle='tab' href='#hinh-anh'>Hình ảnh</a></li> </ul><div class='popup-content tab-content'><div id='thong-tin-sat-lo' class='tab-pane fade in active show'><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Mức độ:</p><p class='p-0 my-2'>$n->level_name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Tên:</p><p class='p-0 my-2'>$n->title</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Vị trí:</p><p class='p-0 my-2'>$n->commune, $n->district, $n->province</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Ghi chú:</p><p class='p-0 my-2'>$n->name</p></div><div class='d-flex'><p class='col-4 p-0 my-2 font-weight-bold'>Chiều dài (m):</p><p class='p-0 my-2'>$n->length</p></div></div><div id='hinh-anh' class='tab-pane fade'><img class='w-100 d-block' style='height:200px;' alt='hinh-anh-sat-lo' src='$n->image' /></div></div>"
                     ],
                     'id' => $n->gid
                 ]);
@@ -103,5 +121,14 @@ class LandslideLocationsController extends Controller
         $landSlideLocations = LandslideLocations::where('year', 2020)->get();
 
         return view('pages.hien-trang-sat-lo-2020', ['normalJson' => $normalJson, 'dangerJson' => $dangerJson, 'veryDangerJson' => $veryDangerJson, 'landSlideLocations' => $landSlideLocations ]);
+    }
+
+    function checkImageExist($image)
+    {
+        if($image == '')
+        {
+            return 'd-none';
+        }
+        return 'd-block';
     }
 }
