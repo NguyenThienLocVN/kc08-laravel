d69797433182dd2014855141ab038585e00f5df4
Loc Nguyen <kten1112@gmail.com>
2020-09-27 20:39:48 +0700

    Fixed overlay popup

--- resources/views/components/meteorology-modal.blade.php
+++ resources/views/components/meteorology-modal.blade.php
@@ -3,6 +3,7 @@
      aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
+            <div id="overlay"></div>
             <div class="modal-header">
                 <h5 class="modal-title font-weight-bold" id="meteorologyModalLabel"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
