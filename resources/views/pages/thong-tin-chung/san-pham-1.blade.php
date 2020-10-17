2db09477ca0770a65953eabed608619d0355331a
root <kten1112@gmail.com>
2020-10-17 04:05:47 +0000

    Add file san pham de tai

---
+++ resources/views/pages/thong-tin-chung/san-pham-1.blade.php
@@ -0,0 +1,9 @@
+@section('title', 'Mục tiêu chung | Chương trình KC.08/16-20')
+@extends('layouts.base')
+
+@section('content')
+<div class="content common-target">
+	<h3 class="title"><i class="fa fa-circle" aria-hidden="true"></i>&nbsp;Mục tiêu chung</h3>
+	<iframe src="{{ asset('files/san-pham-1.pdf') }}" style="width:100%;height:600px;"></iframe>    
+</div>
+@endsection('content')
